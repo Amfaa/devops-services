20a82073719105c7988c524e35acd4dcd3dccbbd
Syed Mazhar Ali <87525103+Amfaa@users.noreply.github.com>
2023-09-19 22:03:54 +0530

    final tested app.js

--- s3/app.js
+++ s3/app.js
@@ -1,65 +1,63 @@
-//provides stronger error checking and more secure code
 "use strict";
 
-// web server framework for Node.js applications
 const express = require("express");
-// allows requests from web pages hosted on other domains
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 const fileName = "app.js";
-
+const axios = require("axios"); // Import the axios library
 const { HttpException } = require("./HttpException.utils");
 
 const app = express();
 const port = 8083;
 
-// Parses incoming JSON requests
 app.use(express.json());
 app.use(cors());
 
-/** add reqId to api call */
 app.use(function (req, res, next) {
   res.locals.reqId = uuidv4();
   next();
 });
 
-app.post("/add-sub", (req, res) => {
-  const {a=0, b=0} = req.body;
+app.post("/add-sub", async (req, res) => {
+  const { a = 0, b = 0 } = req.body;
   console.log(`A: ${a}, B: ${b}`);
 
-  //////////////////////////////////////
-  // Your logic to call S1 and S2 services to get the addition and subtraction
-  //////////////////////////////////////
-try {
+  
+               // My Logic
+  try {
     // Replace these URLs with the actual endpoints of your S1 and S2 services
-    const s1Url = "http://localhost:8081/add"; // Example URL http://s1.ServerIP:PortNo/route
-    const s2Url = "http://localhost:8082/sub"; // Example URL http://s2.ServerIP:PortNo/route
+    const s1Url = process.env.S1_URL // Example URL
+    const s2Url = process.env.S2_URL // Example URL
+    console.log(s1Url, s2Url)
 
     // Make HTTP requests to S1 and S2 services
-    const responseS1 = await axios.get(s1Url, {
-      params: {
-        a: a,
-        b: b,
-      },
+    const responseS1 = await axios.post(s1Url, {
+      a: a,
+      b: b,
     });
+    console.log(responseS1)
 
-    const responseS2 = await axios.get(s2Url, {
-      params: {
+    const responseS2 = await axios.post(s2Url, {
         a: a,
         b: b,
-      },
     });
+    console.log(responseS2)
 
     // Calculate addition and subtraction based on the responses
-    const additionResult = responseS1.data.result;
-    const subtractionResult = responseS2.data.result;
+    const additionResult = responseS1.data;
+    const subtractionResult = responseS2.data;
 
     res.json({
       sum: additionResult,
       difference: subtractionResult,
+    });
+  } catch (error) {
+    // Handle errors here
+    console.error("Error calling services:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-/** 404 error */
 app.all("*", (req, res, next) => {
   const err = new HttpException(404, "Endpoint Not Found");
   return res.status(err.status).send(err.message);
